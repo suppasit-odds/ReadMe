2a85604bb0f9e9ef8e30b6c3b9c3a50ac4556110
Suppasit Beer <82632771+suppasit-odds@users.noreply.github.com>
2022-10-08 06:30:14 +0700

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Suppasit beer on 8/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
